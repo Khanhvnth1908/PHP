97645df5691796fac5aa9ec2e42200d0bde47a79
Khanhvnth <khanhvuong987@gmail.com>
2020-07-07 14:05:40 +0700

    Gallery Project

---
+++ testLaravel/testLaravel/database/migrations/2020_07_04_071718_create_posts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id('id');
+            $table->string('titleGallery');
+            $table->string('descGallery');
+            $table->string('imgFullNameGallery');
+            $table->integer('orderGallery');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
